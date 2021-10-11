0915354c9f39d49429dc85417ced0ba2e87ed980
Eduardo Barros <eduardo.santos@acbprocessamento.com.br>
2021-10-11 17:50:33 -0300

    add rotas condutor retirada controller automático

--- backend/src/DETRAN.API/Controllers/CondutorController.cs
+++ backend/src/DETRAN.API/Controllers/CondutorController.cs
@@ -1,4 +1,9 @@
-using DETRAN.Persistence.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DETRAN.Application.Services.Interface;
+using DETRAN.Domain.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DETRAN.API.Controllers
@@ -7,9 +12,44 @@ namespace DETRAN.API.Controllers
     [Route("[controller]")]
     public class CondutorController : Controller
     {
-        private readonly DetranContext _context;
-        public CondutorController(DetranContext context){
-            _context = context;
+        private readonly ICondutorService _condutorService;
+
+        public CondutorController(ICondutorService condutorService)
+        {
+            _condutorService = condutorService;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                 var condutores = await _condutorService.GetAllCondutoresAsync(true);
+                if (condutores == null) return NotFound("Nenhum condutor encontrado!");
+                return Ok(condutores);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Error ao tentar recuperar condutor. Erro {ex.Message}");
+            }
+        }
+
+        
+        [HttpGet("{veiculo}/veiculo")]
+        public async Task<IActionResult> GetByVeiculo(string veiculo)
+        {
+            try
+            {
+                 var condutor = await _condutorService.GetAllCondutoresByVeiculoAsync(veiculo, false);
+                if (condutor == null) return NotFound("veículo não encontrado!");
+                return Ok(condutor);
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Error ao tentar recuperar condutor. Erro {ex.Message}");
+            }
+        }
+
+
     }
 }
